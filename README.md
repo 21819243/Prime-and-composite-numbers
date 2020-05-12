a927fb34ddb84ab240c00279a79aedcf185fa9e4
Salih Şahin <slhshntr@gmail.com>
2020-05-12 20:48:15 +0300

    updated js, html and readme file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Prime and composite numbers
- Prime and composite numbers
+
+--------------------------------------------------Example Program Execution----------------------------------------------------------------------
+Please give a number-> 12
+12 is composite number and factors are -> 2,3,4,6,12
+With 1st method number of iteration is: 11
+With 2nd method number of iteration is: 3
+
+Please give a number-> 101
+101 is prime number and factors are -> 101  
+With 1st method number of iteration is: 100
+With 2nd method number of iteration is: 3
--- index.js
+++ index.js
@@ -1,57 +1,77 @@
+// Project Github Link: https://github.com/21819243/Prime-and-composite-numbers
 function primalityTest() {
-    var selectedNumber = document.getElementById('selected_number').value;
-    var limitedValue = Math.floor(Math.sqrt(document.getElementById('selected_number').value));
+    var selectedNumber = document.getElementById("selected_number").value;
+    var limitedValue = Math.floor(
+        Math.sqrt(document.getElementById("selected_number").value)
+    );
     var i;
     var count = 0;
     var iteration = 0;
 
     for (i = 2; i <= selectedNumber; i++) {
-
-        if (selectedNumber % i == 0)
-            count++;
+        if (selectedNumber % i == 0) count++;
         iteration++;
     }
 
     function numFactors(selectedNumber) {
-
         const result = [];
         for (var j = 2; j <= selectedNumber; j++) {
             while (selectedNumber % j === 0) {
                 if (!result.includes(j)) result.push(j);
-                selectedNumber /= j;
+                j++;
+                // selectedNumber /= j;
             }
         }
         return result;
     }
 
-    function sieveAlgortihm(limitedValue) {
-        var j;
-        var count2 = 0;
-        var iteration2 = 0;
-        for (j = 2; j <= limitedValue; j++) {
-
-            if (limitedValue % j == 0) {
-                iteration2++;
-
-            }
-
+    function sieveAlgortihmPrime(selectedNumber) {
+        var j = 2;
+        var nsqrt = Math.floor(Math.sqrt(selectedNumber));
+        var iterationS = 0;
+        while (j <= nsqrt) {
+            if (nsqrt % j == 0) iterationS++;
+            j++;
         }
-        return iteration2;
+        return iterationS;
     }
 
-
+    function sieveAlgortihmComposite(selectedNumber) {
+        var j = 2;
+        var nsqrt = Math.floor(Math.sqrt(selectedNumber));
+        var iterationS = 0;
+        while (nsqrt < selectedNumber) {
+            if (selectedNumber % nsqrt == 0) iterationS++;
+            nsqrt++;
+        }
+        return iterationS;
+    }
 
     if (count == 1) {
-        document.getElementById('result').innerHTML = "<strong>Prime/Composite: </strong> Prime Number" + "<br>";
-        document.getElementById('result').innerHTML += "<strong>Number of Iteration (1.Method): </strong>  " + iteration + "<br>";
-        document.getElementById('result').innerHTML += "<strong>Number of Iteration (2.Method): </strong>  " + sieveAlgortihm(limitedValue) + "<br>";
-        document.getElementById('result').innerHTML += "<strong>Dividers: </strong>  " + numFactors(selectedNumber);
+        document.getElementById("result").innerHTML =
+            "<strong>Prime/Composite: </strong> Prime Number" + "<br>";
+        document.getElementById("result").innerHTML +=
+            "<strong>Number of Iteration (1.Method): </strong>  " +
+            iteration +
+            "<br>";
+        document.getElementById("result").innerHTML +=
+            "<strong>Number of Iteration (2.Method): </strong>  " +
+            sieveAlgortihmPrime(selectedNumber) +
+            "<br>";
+        document.getElementById("result").innerHTML +=
+            "<strong>Dividers: </strong>  " + numFactors(selectedNumber);
     } else {
-        document.getElementById('result').innerHTML = "<strong>Prime/Composite: </strong> Composite Number" + "<br>";
-        document.getElementById('result').innerHTML += "<strong>Number of Iteration (1.Method): </strong>  " + iteration + "<br>";
-        document.getElementById('result').innerHTML += "<strong>Number of Iteration (2.Method): </strong>  " + sieveAlgortihm(limitedValue) + "<br>";
-        document.getElementById('result').innerHTML += "<strong>Dividers: </strong>  " + numFactors(selectedNumber);
+        document.getElementById("result").innerHTML =
+            "<strong>Prime/Composite: </strong> Composite Number" + "<br>";
+        document.getElementById("result").innerHTML +=
+            "<strong>Number of Iteration (1.Method): </strong>  " +
+            iteration +
+            "<br>";
+        document.getElementById("result").innerHTML +=
+            "<strong>Number of Iteration (2.Method): </strong>  " +
+            sieveAlgortihmComposite(selectedNumber) +
+            "<br>";
+        document.getElementById("result").innerHTML +=
+            "<strong>Dividers: </strong>  " + numFactors(selectedNumber);
     }
-
-
 }
