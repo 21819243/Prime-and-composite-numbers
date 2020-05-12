f11a4cb5e711a1a6a6a255c4918ea23f02597046
Salih Şahin <slhshntr@gmail.com>
2020-05-12 17:57:32 +0300

    second method

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-function method1() {
+function primalityTest() {
     var selectedNumber = document.getElementById('selected_number').value;
-
+    var limitedValue = Math.floor(Math.sqrt(document.getElementById('selected_number').value));
     var i;
     var count = 0;
     var iteration = 0;
@@ -24,15 +24,32 @@ function method1() {
         return result;
     }
 
+    function sieveAlgortihm(limitedValue) {
+        var j;
+        var count2 = 0;
+        var iteration2 = 0;
+        for (j = 2; j <= limitedValue; j++) {
+
+            if (limitedValue % j == 0) {
+                iteration2++;
+
+            }
+
+        }
+        return iteration2;
+    }
+
 
 
     if (count == 1) {
         document.getElementById('result').innerHTML = "<strong>Prime/Composite: </strong> Prime Number" + "<br>";
-        document.getElementById('result').innerHTML += "<strong>Number of Iteration: </strong>  " + iteration + "<br>";
+        document.getElementById('result').innerHTML += "<strong>Number of Iteration (1.Method): </strong>  " + iteration + "<br>";
+        document.getElementById('result').innerHTML += "<strong>Number of Iteration (2.Method): </strong>  " + sieveAlgortihm(limitedValue) + "<br>";
         document.getElementById('result').innerHTML += "<strong>Dividers: </strong>  " + numFactors(selectedNumber);
     } else {
         document.getElementById('result').innerHTML = "<strong>Prime/Composite: </strong> Composite Number" + "<br>";
-        document.getElementById('result').innerHTML += "<strong>Number of Iteration: </strong>  " + iteration + "<br>";
+        document.getElementById('result').innerHTML += "<strong>Number of Iteration (1.Method): </strong>  " + iteration + "<br>";
+        document.getElementById('result').innerHTML += "<strong>Number of Iteration (2.Method): </strong>  " + sieveAlgortihm(limitedValue) + "<br>";
         document.getElementById('result').innerHTML += "<strong>Dividers: </strong>  " + numFactors(selectedNumber);
     }
 
